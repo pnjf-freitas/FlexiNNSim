1b356c1a5f1b11927820a3426285fb3715e2b543
pnjf-freitas <pnjf.freitas@gmail.com>
2022-10-19 15:27:03 +0100

    First working script for Inferring Lookup Table from C2CStruct. Results do not much with does extracted from data

--- utils/VariabilityAnalysis/Infer_Hwang_model_from_GS.m
+++ utils/VariabilityAnalysis/Infer_Hwang_model_from_GS.m
@@ -1,4 +1,4 @@
-clear all;
+clearvars;
 close all;
 clc;
 
@@ -19,25 +19,44 @@ GS_LTD = (diff(G_LTD, w)/symG_LRS) * 100;
 
 answer = inputdlg(["G_LRS", "G_HRS", "max_GS (%)", "# of pulses"]);
 
-G_LRS = str2num(answer{1});
-G_HRS = str2num(answer{2});
-max_GS = str2num(answer{3});
-n_Pulses = str2num(answer{4});
+G_LRS = str2double(answer{1});
+G_HRS = str2double(answer{2});
+max_GS = str2double(answer{3});
+n_Pulses = str2double(answer{4});
 
 clear answer;
 
 %% Rewrite GS expression
 
-%syms symGS_LTP symGS_LTD;
+syms symGS_LTP symGS_LTD;
 
 num_w = 1/n_Pulses;
 
-GS_LTP_eq = eq(max_GS, eliminate(subs(GS_LTP, [symG_LRS, symG_HRS, w], [G_LRS, G_HRS, num_w]), [symG_LRS, symG_HRS, w]));
-GS_LTD_eq = eq(-max_GS, eliminate(subs(GS_LTD, [symG_LRS, symG_HRS, w], [G_LRS, G_HRS, num_w]), [symG_LRS, symG_HRS, w]));
+GS_LTP_eq = eq(symGS_LTP, eliminate(subs(GS_LTP, [symG_LRS, symG_HRS], [G_LRS, G_HRS]), [symG_LRS, symG_HRS]));
+GS_LTD_eq = eq(-symGS_LTD, eliminate(subs(GS_LTD, [symG_LRS, symG_HRS], [G_LRS, G_HRS]), [symG_LRS, symG_HRS]));
 
-num_alpha_p = vpasolve(GS_LTP_eq, symalpha_p);
-num_alpha_d = vpasolve(GS_LTD_eq, symalpha_d);
+num_alpha_p = double( vpasolve(eliminate(subs(GS_LTP_eq, [symGS_LTP, w], [max_GS, num_w]), [symGS_LTP, w]), symalpha_p) );
+num_alpha_d = double( vpasolve(eliminate(subs(GS_LTD_eq, [symGS_LTD, w], [max_GS, num_w]), [symGS_LTD, w]), symalpha_d) );
 
 %% Plot generated curves
 
-%LTP_Model = eq(G, eliminate(subs(G_LTP, [symG_LRS, symG_HRS, symalpha_p], [G_LRS, G_HRS, num_alpha_p]), [symG_LRS, symG_HRS, symalpha_p]));
+LTP_Model = eq(G, eliminate(subs(G_LTP, [symG_LRS, symG_HRS, symalpha_p], [G_LRS, G_HRS, num_alpha_p]), [symG_LRS, symG_HRS, symalpha_p]));
+LTD_Model = eq(G, eliminate(subs(G_LTD, [symG_LRS, symG_HRS, symalpha_d], [G_LRS, G_HRS, num_alpha_d]), [symG_LRS, symG_HRS, symalpha_d]));
+
+
+% Figure 1 - Conductance vs Normalized Weight
+fig1 = figure;
+
+fimplicit(LTP_Model, [0,1], '-r');
+ylim([G_HRS, G_LRS]);
+
+hold on;
+fimplicit(LTD_Model, [0,1], '-b');
+
+ylabel('G (S)');
+xlabel('Normalized Weight');
+
+% Figure 2 - GS vs Normalized Weight
+%fig2 = figure;
+
+%fimplicit()
